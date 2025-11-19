390fb93427bf21e3b64a106f827097876b6dc4b5
simon <75259597+tzimom@users.noreply.github.com>
2025-11-19 08:04:15 +0100

    Initial setup

---
+++ config.fish
@@ -0,0 +1,13 @@
+eval (/opt/homebrew/bin/brew shellenv)
+
+fish_vi_key_bindings
+
+alias ls 'eza --color=always --classify=always --hyperlink'
+alias l 'ls --long --icons=always'
+alias la 'l --all'
+alias lt 'l --tree'
+alias lta 'l --tree --all'
+
+alias tt 'ttyper --words=20'
+
+zoxide init fish --cmd cd | source
---
+++ functions/fish_prompt.fish
@@ -0,0 +1,19 @@
+function fish_prompt
+    set_color brblack
+    echo -n "[$(date '+%H:%M')] "
+    set_color blue
+    echo -n (whoami)@(prompt_hostname)
+
+    if [ $PWD != $HOME ]
+        set_color brblack
+        echo -n ':'
+        set_color yellow
+        echo -n (basename $PWD)
+    end
+
+    set_color green
+    printf '%s ' (__fish_git_prompt)
+    set_color red
+    echo -n '| '
+    set_color normal
+end
