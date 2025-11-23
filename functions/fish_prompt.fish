2dadb95385b3e924a208634583b136dda01d5465
simon <75259597+tzimom@users.noreply.github.com>
2025-11-23 23:03:08 +0100

    Make fish prompt show ~ in home directory

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,19 +1,12 @@
 function fish_prompt
-    set_color brblack
-    echo -n "[$(date '+%H:%M')] "
-    set_color blue
-    echo -n (whoami)@(prompt_hostname)
+    set_color brblack;  echo -n "[$(date '+%H:%M')] ";
+    set_color blue;     echo -n (whoami)@(prompt_hostname);
 
-    if [ $PWD != $HOME ]
-        set_color brblack
-        echo -n ':'
-        set_color yellow
-        echo -n (basename $PWD)
-    end
+    set_color brblack;  echo -n ':';
+    set_color yellow;   echo -n (if [ $PWD != $HOME ]; basename $PWD; else; echo "~"; end);
 
-    set_color green
-    printf '%s ' (__fish_git_prompt)
-    set_color red
-    echo -n '| '
-    set_color normal
+    set_color green;    printf '%s ' (__fish_git_prompt);
+    set_color red;      echo -n '| ';
+
+    set_color normal;
 end
